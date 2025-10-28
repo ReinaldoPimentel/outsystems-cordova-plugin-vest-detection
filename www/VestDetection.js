ef4399778ef70fe97820177c8e3ce5e084064087
js7vensousa <joao.sousa@acceleratedfocus.com>
2025-10-28 20:57:12 -0100

    Add logging and improve error handling in detectVest function of VestDetection.js

--- www/VestDetection.js
+++ www/VestDetection.js
@@ -8,19 +8,36 @@ module.exports = {
      * @param {function} errorCallback - Error callback
      */
     detectVest: function(base64Image, successCallback, errorCallback) {
+        console.log('[VestDetection.js] detectVest called');
+        console.log('[VestDetection.js] Arguments:', arguments);
+        
         if (typeof base64Image !== 'string' || !base64Image) {
+            console.log('[VestDetection.js] Invalid image - calling error callback');
             if (errorCallback) {
                 errorCallback('Invalid base64 image provided');
             }
             return;
         }
 
-        exec(
-            successCallback,
-            errorCallback,
-            'VestDetection',
-            'detectVest',
-            [base64Image]
-        );
+        console.log('[VestDetection.js] About to call exec');
+        try {
+            exec(
+                function(result) {
+                    console.log('[VestDetection.js] Success from native:', result);
+                    if (successCallback) successCallback(result);
+                },
+                function(error) {
+                    console.log('[VestDetection.js] Error from native:', error);
+                    if (errorCallback) errorCallback(error);
+                },
+                'VestDetection',
+                'detectVest',
+                [base64Image]
+            );
+            console.log('[VestDetection.js] exec call completed');
+        } catch(e) {
+            console.log('[VestDetection.js] EXCEPTION in exec:', e);
+            if (errorCallback) errorCallback('Exception: ' + e.message);
+        }
     }
 };
